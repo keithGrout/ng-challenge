74966e2891a79c13dbf41ffc14fbcb72a5019a1e
keithGrout <keiwi.g@gmail.com>
2014-06-04 21:28:49 -0700

    added routes, not working

--- day1/script.js
+++ day1/script.js
@@ -1,10 +1,41 @@
 // create the module and name it scotchApp
-var scotchApp = angular.module('scotchApp', []);
+var scotchApp = angular.module('scotchApp', ['ngRoute']);
+
+// configure our routes
+scotchApp.config(function($routeProvider){
+	$routeProvider
+
+	// route for the homepage
+	.when('/', {
+		templateURL: 'pages/home.html',
+		controller: 'mainController'
+	})
+
+	// route for the about page
+	.when('/about', {
+		templateURL: 'pages/about.html',
+		controller: 'aboutController'
+	})
+
+	// route for the contact page
+	.when('/contact', {
+		templateURL: 'pages/contact.html',
+		controller: 'contactController'
+	});
+})
 
 // create the controller and inject Angular's $scope
-scotchApp.controller("mainController", function($scope){
 
-	// create a message to display in the view
-	$scope.message = "keith was here";
+	scotchApp.controller('mainController', function($scope) {
+		// create a message to display in our view
+		$scope.message = 'Everyone come and see how good I look!';
+	});
 
-})
+	scotchApp.controller('aboutController', function($scope) {
+		$scope.message = 'Look! I am an about page.';
+	});
+
+	scotchApp.controller('contactController', function($scope) {
+		$scope.message = 'Contact us! JK. This is just a demo.';
+	});
+	
